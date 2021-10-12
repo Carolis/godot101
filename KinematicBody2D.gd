eb46d3748caaf2289219261b62a65895b8570ee7
Carolis <redrumjedi@gmail.com>
2021-10-12 13:46:37 -0300

    add "gravity"

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,13 +2,16 @@ extends KinematicBody2D
 
 var motion = Vector2()
 
+
 func _physics_process(delta):
-	
+	motion.y += 100
+
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 1000	
+		motion.x = 100
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -1000
-	else: motion.x = 0
-	
+		motion.x = -100
+	else:
+		motion.x = 0
+
 	move_and_slide(motion)
 	pass
