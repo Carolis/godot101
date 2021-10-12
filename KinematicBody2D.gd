dc56c213e91eb2e2e6ba1df3a9ef15d40f4ef29c
Carolis <redrumjedi@gmail.com>
2021-10-12 15:00:14 -0300

    add some const variables

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,20 +3,24 @@ extends KinematicBody2D
 var UP = Vector2(0, -1)
 var motion = Vector2()
 
+var SPEED = 300
+var GRAVITY = 22
+var JUMP_HEIGHT = 500
+
 
 func _physics_process(delta):
-	motion.y += 22
+	motion.y += GRAVITY
 
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -SPEED
 	else:
 		motion.x = 0
 
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -400
+			motion.y = -JUMP_HEIGHT
 
 	motion = move_and_slide(motion, UP)
 	pass
